869f44c0f585caa97d49208ef3224fcd8cac59a8
Adam Morris <thatadammorris@gmail.com>
2023-04-24 14:43:38 -0400

    More sim tests

--- data-analysis/model-fitting/runSimulations.R
+++ data-analysis/model-fitting/runSimulations.R
@@ -12,7 +12,7 @@ numTrials = 100
 source(paste0(here(),'/model-fitting.R')) # get right filepath for this
 load(paste0(here(),'/option_diffs.rdata')) # get right filepath for this
 
-numSubj = 100
+numSubj = 250
 numCores = 32
 
 models_to_simulate = 1
@@ -43,34 +43,6 @@ for (simulating_model in models_to_simulate) {
     if (simulating_model %in% c(2,4,6)) {option_diffs_touse_subj = sign(option_diffs_touse_subj)}
     choices_touse[[subj]] = generateChoices(agent_inv_temps[subj], agent_weights[subj,], option_diffs_touse_subj)
   }
-  
-  #debugging
-  # test <- fitSubj(stan_model_binwts, option_diffs_touse[[subj]], choices_touse[[subj]], 0, 1, all_combinations_binwts)
-  # weights = agent_weights[subj,]
-  # utilities_untemped = as.vector(weights %*% option_diffs_touse[[subj]])
-  # utilities_num_untemped = utilities_untemped
-  # utilities_num_untemped[choices_touse[[subj]] == 0] = 0
-  # 
-  # begin = Sys.time()
-  # stan_fit <- sampling(stan_model_binwts,
-  #                                data = list(numChoices = 100,
-  #                                            utilities_untemped = utilities_untemped,
-  #                                            utilities_num_untemped = utilities_num_untemped),
-  #                                iter = numIter,
-  #                                chains = numChains,
-  #                                seed = stan.seed,
-  #                                refresh = 0)
-  # stan_lme = bridge_sampler(stan_fit, silent = T)$logml
-  # Sys.time() - begin
-  # 
-  # 
-  # f_creator = function(u1, u2) {return(function(x) {dgamma(x, 4) * rowProds(exp(x %*% t(u1)) / (1+exp(x %*% t(u2))))})}
-  # 
-  # begin = Sys.time()
-  # f = f_creator(utilities_num_untemped, utilities_untemped)
-  # log(integrate(f, 0, 20)$value)
-  # test = optimize(f, c(0,20), maximum = T)
-  # Sys.time() - begin
   
   fitting_results = mclapply(
     1:numSubj,
